74b16162349e1fd15438e916141eccd7c1503b74
amejid <abdelmejid89@gmail.com>
2023-01-29 22:09:15 +0300

    Create a websocket connection

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+io.on('connection', () => {
+  console.log('New websocket connection');
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log(`SERVER started on ${port}`));
